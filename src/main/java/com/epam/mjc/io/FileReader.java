49e84e082711d98332c9b1fdf87a3efaa82185fa
raman <r4m4n911@gmail.com>
2025-07-17 21:29:05 +0300

    12

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,45 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+//import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String str = "";
+        try {
+            str = Files.readString(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] arr = str.split("\n");
+        String[] m = new String[4];
+        for (int i = 0; i < 4; i++) {
+            String[] keyvalue = arr[i].split(":");
+            m[i] = keyvalue[1];
+        }
+        int age;
+        try {
+            age= Integer.parseInt(m[1]);
+        } catch (NumberFormatException e) {
+            age = 0;
+        }
+        long t;
+        try {
+            t = Long.parseLong(m[3]);
+        } catch (NumberFormatException e) {
+            t = 0;
+        }
+        return new Profile(m[0], age, m[2], t);
     }
+   public static void main(String[] args) {
+    getDataFromFile("Profile.txt");
+   }
 }
